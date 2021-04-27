dce7307b1ae22d4042ac4eb877239ddd8b4212e9
Klionovska, Ksenia <ksenia.klionovska@dlr.de>
2021-04-27 09:09:07 +0200

    change time of traiding

--- DolphRobot.py
+++ DolphRobot.py
@@ -34,7 +34,7 @@ class Dolph:
 
         self.numTestSample = 1300
         self.since = dt.date(year=2020    ,month=2,day=1)
-        self.between_time = ('10:00', '23:50')
+        self.between_time = ('07:00', '23:50')
         self.TrainingHour = 10
     
         if self.MODE == 'TRAIN_OFFLINE' or self.MODE == 'TEST_OFFLINE':
