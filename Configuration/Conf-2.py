658c9cdd27e118ef0f16e1ac4690b964c85868d4
Klionovska <Ksenia.Klionovska@dlr.de>
2025-03-12 21:03:56 +0100

    chnage coeff for paraavariaz

--- Configuration/Conf-2.py
+++ Configuration/Conf-2.py
@@ -20,9 +20,9 @@ securities = [
             'exitTimeSeconds': 36000,
             'entryTimeSeconds': 3600,
             'minNumPastSamples': 51
-            ,"longPositionMargin": 0.0035 
-            ,"shortPositionMargin": 0.0035 
-            ,"stopLossCoefficient": 3 
+            ,"longPositionMargin": 0.001 
+            ,"shortPositionMargin": 0.001 
+            ,"stopLossCoefficient": 1
             ,"acceptableTrainingError": 0.000192
             ,'period': '1Min'
         }
@@ -31,7 +31,7 @@ securities = [
         'seccode': 'INTC'       
         ,'board': 'EQTY'
         ,'market': 'NASDAQ'
-        ,'decimals' : 3
+        ,'decimals' : 2
         ,'id' : 0
         ,'params': {
             'algorithm': 'stochastic_and_rsi',
@@ -39,9 +39,9 @@ securities = [
             'exitTimeSeconds': 36000,
             'entryTimeSeconds': 3600,
             'minNumPastSamples': 51
-            ,"longPositionMargin": 0.0035 
-            ,"shortPositionMargin": 0.0035 
-            ,"stopLossCoefficient": 3 
+            ,"longPositionMargin": 0.001
+            ,"shortPositionMargin": 0.001
+            ,"stopLossCoefficient": 1 
             ,"acceptableTrainingError": 0.000192
             ,'period': '1Min'
         }
@@ -58,9 +58,9 @@ securities = [
             'exitTimeSeconds': 36000,
             'entryTimeSeconds': 3600,
             'minNumPastSamples': 51
-            ,"longPositionMargin": 0.0035 
-            ,"shortPositionMargin": 0.0035 
-            ,"stopLossCoefficient": 3 
+            ,"longPositionMargin": 0.001
+            ,"shortPositionMargin": 0.001
+            ,"stopLossCoefficient": 1
             ,"acceptableTrainingError": 0.000192
             ,'period': '1Min'
         }
@@ -108,10 +108,10 @@ statusStopOrderFilled = ['filled','Filled']
 
 ########### default-fallback values ##########################################
 factorPosition_Balance = 0.3
-factorMargin_Position  = 0.0035
+factorMargin_Position  = 0.001
 entryTimeSeconds = 3600
 exitTimeSeconds = 36000
-stopLossCoefficient = 3
+stopLossCoefficient = 1
 correction = 0.0
 spread = 0.0
 
