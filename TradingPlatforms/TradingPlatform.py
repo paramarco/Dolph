5774ec3b46256cd23065ceae03747d3188f60e4d
paramarco <4086177+paramarco@users.noreply.github.com>
2026-02-11 18:20:27 +0100

    subscribe_to_market_data() to 60 Seconds

--- TradingPlatforms/TradingPlatform.py
+++ TradingPlatforms/TradingPlatform.py
@@ -1679,7 +1679,7 @@ class IBTradingPlatform(TradingPlatform):
             bars = self.ib.reqHistoricalData(
                 contract,
                 endDateTime='',
-                durationStr='300 S',
+                durationStr='60 S',
                 barSizeSetting='1 min',
                 whatToShow='TRADES',
                 useRTH=False,
